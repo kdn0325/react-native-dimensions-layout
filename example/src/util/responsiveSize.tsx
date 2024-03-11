b9f4334d1c4ee0cb8da6e5ef771db4111ff6d864
kdn0325 <dn10003@gmail.com>
2024-03-11 21:02:01 +0900

    docs: add Example Annotations

--- example/src/util/responsiveSize.tsx
+++ example/src/util/responsiveSize.tsx
@@ -4,21 +4,25 @@ import {
   percentBaseWidth,
 } from 'react-native-dimensions-util';
 
+// Screen size defined in your Figma design
 const FIGMA_WINDOW_WIDTH = 375;
 const FIGMA_WINDOW_HEIGHT = 812;
 
+// function that adjusts the width based on the size of the Figma window
 export function widthPercentage(width: number): number {
   const percentage = (width / FIGMA_WINDOW_WIDTH) * 100;
 
   return percentBaseWidth(percentage);
 }
 
+//  function that adjusts the height based on the size of the Figma window
 export function heightPercentage(height: number): number {
   const percentage = (height / FIGMA_WINDOW_HEIGHT) * 100;
 
   return percentBaseHeight(percentage);
 }
 
+// ffunction to adjust font size based on Figma window size
 export function fontPercentage(size: number): number {
   const percentage = size * 0.135;
 
